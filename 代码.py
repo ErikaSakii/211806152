3f606b4acb20ad2fe0ffa1f47afb9a9bbdb6209b
ErikaSakii <45238475+ErikaSakii@users.noreply.github.com>
2019-11-18 22:20:51 +0800

    Update 代码.py

--- 代码.py
+++ 代码.py
@@ -1,20 +1,21 @@
+from collections import Counter
 txt1 = open("text.txt",'r') #打开文件
 hackers = [] #建立用于放异常用户的列表
 for line in txt : 
 a = line.strip().split(' ') #把文件里的数据 按行分开
-count = 0 #计用户发送数据包的次数
+Counter = 0 #计用户发送数据包的次数
 t = 0 
 for b in a : #循环计数
-if b in hackers :
-continue
-b = a[t]
-t += 1
-count += 1
-if count >= 100
-hackers.append(b) 
+    if b in hackers :
+       continue
+    b = a[t]
+    t += 1
+    Counter += 1
+    if Counter >= 100
+       hackers.append(b) 
 c = len(hackers) #异常用户个数
 txt2 = open("text.txt",'w') #写入txt2 
 txt2.write(count + '\n')
 for d in hackers :
-txt2.write(d + '\n')
-txt2.close() 
+    txt2.write(d + '\n')
+    txt2.close() 
